1c1b973025db4b32fa1d8521807a479ed384c38c
mwaiowen <mwaiowen.m@gmail.com>
2024-06-19 09:47:32 +0300

    Deleted the old client and server folder

--- server/routes/routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var auth_1 = require("./auth");
-var rootRouter = (0, express_1.Router)();
-rootRouter.use("/auth", auth_1.default);
-exports.default = rootRouter;
